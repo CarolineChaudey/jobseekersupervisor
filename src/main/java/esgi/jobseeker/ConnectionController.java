b0f3c03bb095b516ec1be582fc57e7d0d924b166
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-26 17:21:01 +0200

    changement fenetre

--- src/main/java/esgi/jobseeker/ConnectionController.java
+++ src/main/java/esgi/jobseeker/ConnectionController.java
@@ -1,9 +1,18 @@
 package esgi.jobseeker;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class ConnectionController {
     @FXML
@@ -19,11 +28,30 @@ public class ConnectionController {
                     pswdField.getText());
             if (result) {
                 System.out.println("Success !");
+                changeWindow();
             } else {
                 System.out.println("Incorrect credentials.");
             }
         } catch (Exception e) {
             System.out.println("Error while connecting.");
+        }
+    }
+
+    private void changeWindow() {
+        System.out.println("in changeWindow");
+        Parent root;
+        try {
+
+            root = FXMLLoader.load(getClass().getResource("/fxml/test.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("JobSeekerSupervisor");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            Stage newStage = (Stage) ((Node)(this.loginField)).getScene().getWindow();
+            newStage.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
