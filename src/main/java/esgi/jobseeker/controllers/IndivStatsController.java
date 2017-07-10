72e2f891cc9f5131e72a06769709561c95831829
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-07-10 23:25:48 +0200

    combo box

--- src/main/java/esgi/jobseeker/controllers/IndivStatsController.java
+++ src/main/java/esgi/jobseeker/controllers/IndivStatsController.java
@@ -1,7 +1,24 @@
 package esgi.jobseeker.controllers;
 
+import esgi.jobseeker.WebserviceConnector;
+import esgi.jobseeker.model.Seeker;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.util.List;
+
 /**
  * Created by caroline on 10/07/17.
  */
 public class IndivStatsController {
+    @FXML
+    private ComboBox<Seeker> seekerBox;
+
+    @FXML
+    public void initialize() throws Exception {
+        List<Seeker> seekers = WebserviceConnector.getInstance().getSupervisorSeekers();
+        System.out.println(seekers);
+        seekerBox.setItems(FXCollections.observableList(seekers));
+    }
 }
---
+++ src/main/java/esgi/jobseeker/model/QuantityPerDate.java
@@ -0,0 +1,40 @@
+package esgi.jobseeker.model;
+
+import java.util.Date;
+
+/**
+ * Created by caroline on 06/07/17.
+ */
+public class QuantityPerDate {
+    private Date day;
+    private Integer nb;
+
+    public QuantityPerDate(Date day, Integer nb) {
+        this.day = day;
+        this.nb = nb;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public Integer getNb() {
+        return nb;
+    }
+
+    public void setNb(Integer nb) {
+        this.nb = nb;
+    }
+
+    @Override
+    public String toString() {
+        return "QuantityPerDate{" +
+                "day=" + day +
+                ", nb=" + nb +
+                '}';
+    }
+}
